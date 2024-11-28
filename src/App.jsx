4397ef43de74b46bd3dbb57677303eb4040db21b
braimohigboibroheem <braimohigboibroheem@gmail.com>
2024-11-28 06:55:14 -0800

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Components/Static/Header";
 import Hero from "./Components/Pages/hero/Hero";
 import Aboutme from "./Components/Pages/about/Aboutme";
+import Services from "./Components/services/Services";
+import MyWork from "./Components/myWork/MyWork";
 
 
 const App = () => {
@@ -10,6 +12,8 @@ const App = () => {
 <Header/>
 <Hero/>
 <Aboutme/>
+<Services/>
+<MyWork/>
     </div>
   )
 }
